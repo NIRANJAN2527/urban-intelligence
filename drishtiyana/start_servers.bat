5c27d432e68e5f77c7bdb8dc2a7c892695e785a8
NIRANJAN2527 <niranjankammalapally@gmail.com>
2026-09-08 04:17:11 +0530

    docs: add Admin Portal URL to server launcher script

--- drishtiyana/start_servers.bat
+++ drishtiyana/start_servers.bat
@@ -19,6 +19,7 @@ start "DRISHTIYANA Node Server (Ports 3000 & 3001)" cmd /k "cd /d "%~dp0server" 
 echo.
 echo ========================================================
 echo All DRISHTIYANA servers are running in separate windows!
+echo - Admin Portal : http://localhost:3000/admin
 echo - Laptop Viewer: http://localhost:3000/viewer
 echo - Phone Stream : Check Node.js terminal for mobile HTTPS URL
 echo ========================================================
